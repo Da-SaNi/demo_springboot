da777efb29ebb20eef90d1c8fb3034ba9b212be0
Da-SaNi <sjbejdidn@gmail.com>
2024-03-04 01:19:23 +0900

    webhook test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/sonar-test")
+    @GetMapping("/sonar-tes")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
