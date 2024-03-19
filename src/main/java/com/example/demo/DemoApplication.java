a0ba2e66856d321ffdc3998a47953ec6cec61161
Da-SaNi <sjbejdidn@gmail.com>
2024-03-19 23:21:13 +0900

    dsfsdfds

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/testaa")
+    @GetMapping("/testab")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
