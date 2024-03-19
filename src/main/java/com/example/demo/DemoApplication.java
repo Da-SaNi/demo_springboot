b3086044b4541fd2c2dab14442925bdd9412da1e
Da-SaNi <sjbejdidn@gmail.com>
2024-03-19 21:51:00 +0900

    webhook testsdfsdf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/testss")
+    @GetMapping("/testss55")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
